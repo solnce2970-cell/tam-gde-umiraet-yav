d9257fdb3ca8e05cf9967ce4e9d9419bb203ec84
solnce2970-cell <solnce2970@gmail.com>
2026-08-12 21:22:03 +0700

    Enable resilient image loading on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import BackToTop from "./BackToTop";
 import MusicPlayerPortal from "./MusicPlayerPortal";
+import ImageFixer from "./ImageFixer";
 
 export const metadata: Metadata = {
   title: "Там, где умирает Явь",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="ru">
       <body>
         {children}
+        <ImageFixer />
         <MusicPlayerPortal />
         <BackToTop />
       </body>
