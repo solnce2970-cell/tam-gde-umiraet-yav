5b2c217f38929685fab6aee7ee9b84b137ffdea3
solnce2970-cell <solnce2970@gmail.com>
2026-08-14 01:59:20 +0700

    Prepare template bridge mount

--- app/template.tsx
+++ app/template.tsx
@@ -1,7 +1,9 @@
 import { createElement } from "react";
 import MezhaSound from "./MezhaSound";
+import Bridge from "./Bridge";
 
 void MezhaSound;
+void Bridge;
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
