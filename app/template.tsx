b12de53f3c928b48f61f479e14082c422e4d0c67
solnce2970-cell <solnce2970@gmail.com>
2026-08-31 02:52:52 +0700

    Remove obsolete Night Nav click gate

--- app/template.tsx
+++ app/template.tsx
@@ -2,14 +2,12 @@ import Bridge from "./Bridge";
 import NavigationMemory from "./NavigationMemory";
 import WrongWayStar from "./WrongWayStar";
 import MavkiWaterWhisper from "./MavkiWaterWhisper";
-import NightNavClickGate from "./NightNavClickGate";
 import VasiliskCatRevenge from "./VasiliskCatRevenge";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      <NightNavClickGate />
       <Bridge />
       <NavigationMemory />
       <WrongWayStar />
