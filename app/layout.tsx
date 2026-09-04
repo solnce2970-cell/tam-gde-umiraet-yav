410e01664be5d1310fc4f4866002c03ed6a33b6f
solnce2970-cell <solnce2970@gmail.com>
2026-09-05 01:35:49 +0700

    Enable reading links and homepage section order

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import AnomalyDebugPanel from "./AnomalyDebugPanel";
 import SignFoundReveal from "./SignFoundReveal";
 import GlobalFooter from "./GlobalFooter";
 import AmbientAnomalies from "./AmbientAnomalies";
+import ReadingAccessEnhancer from "./ReadingAccessEnhancer";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.tamgdeumiraetyav.ru"),
@@ -53,6 +54,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <ShishigaTrack />
         <SignFoundReveal />
         <AnomalyDebugPanel />
+        <ReadingAccessEnhancer />
       </body>
     </html>
   );
