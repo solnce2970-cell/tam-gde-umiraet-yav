493301b4f035f19517bdd05f4c192fe5d3bc930b
solnce2970-cell <solnce2970@gmail.com>
2026-09-18 20:29:03 +0700

    Add Pravnik to global navigation

--- app/GlobalHeader.tsx
+++ app/GlobalHeader.tsx
@@ -6,6 +6,7 @@ const links = [
   { href: "/chitat", label: "Читать" },
   { href: "/o-romane", label: "О романе" },
   { href: "/#navnik", label: "Навник" },
+  { href: "/pravnik", label: "Правник" },
   { href: "/#characters", label: "Герои" },
   { href: "/genealogy#gods-title", label: "Лики богов" },
   { href: "/#music", label: "Музыка" },
